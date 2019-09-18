2f6bf93ea7ef0afa6f0c631c6bcb4cfc008d2c6c
joldan95 <malf95@hotmail.com>
2019-09-18 22:20:14 +0000

    [Update]Fix returned value at main function

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,10 @@
 /**
  * main - Entry point of the program
  *
- * Return: Always 0 (Success)
+ * Return: 1 (Error)
  */
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (0);
+	return (1);
 }
