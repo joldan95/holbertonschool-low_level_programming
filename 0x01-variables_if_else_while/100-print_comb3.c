df573cd7c193366da14292f1da4e4c2aa0b150f4
joldan95 <malf95@hotmail.com>
2019-09-19 17:42:55 +0000

    [Add]Complete Task 11 Project 1

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of application
+ *
+ * Description: Prints out the decimal
+ * numbers with differents combinations
+ * from 01 to 89 separeted by a comma
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	int m;
+
+	n = 48;
+
+	while (n < 58)
+	{
+		m = n+1;
+			while (m < 58)
+			{
+				putchar(n);
+				putchar(m++);
+				if (m != 58 || n != 56)
+				{
+					putchar(44);
+					putchar(32);
+				}
+			}
+		n++;
+	}
+	putchar(10);
+	return (0);
+}
