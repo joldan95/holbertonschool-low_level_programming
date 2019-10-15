5196d61407469956045a0d0eabfe00ea5fd8c5d1
joldan95 <malf95@hotmail.com>
2019-10-15 21:55:26 +0000

    [Update]Fix the use of function and change the name of function to free memory

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 
-void free_grid(int **grid, int height);
+void free_gr(int **grid, int height);
 
 /**
  * alloc_grid - Returns a pointer to a 2 dimensional array
@@ -28,7 +28,7 @@ int **alloc_grid(int width, int height)
 			p[i] = (int *)malloc(width * sizeof(int));
 			if (p[i] ==NULL)
 			{
-				free_grid(grid, i - 1);
+				free_gr(p, i - 1);
 				return (NULL);
 			}
 			for (j = 0; j < width; j++)
@@ -40,13 +40,13 @@ int **alloc_grid(int width, int height)
 }
 
 /**
- * free_grid - Frees a 2 dimensional grid created with 'alloc_grid'
+ * free_gr - Frees a 2 dimensional grid created with 'alloc_grid'
  * @grid: Pointer to grid to free
  * @height: Number of rows of the array
  *
  * Return: Nothing
  */
-void free_grid(int **grid, int height)
+void free_gr(int **grid, int height)
 {
 	int i;
 
