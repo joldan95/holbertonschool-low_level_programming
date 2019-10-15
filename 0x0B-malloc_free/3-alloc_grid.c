17e1f5e2fb44237e7c04ffba2d8ebb1460b7cfba
joldan95 <malf95@hotmail.com>
2019-10-15 21:47:07 +0000

    [Update]Free memory when can not allocate the columns

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,5 +1,7 @@
 #include <stdlib.h>
 
+void free_grid(int **grid, int height);
+
 /**
  * alloc_grid - Returns a pointer to a 2 dimensional array
  * @width: Number of columns of the array
@@ -24,10 +26,32 @@ int **alloc_grid(int width, int height)
 		for (i = 0; i < height; i++)
 		{
 			p[i] = (int *)malloc(width * sizeof(int));
+			if (p[i] ==NULL)
+			{
+				free_grid(grid, i - 1);
+				return (NULL);
+			}
 			for (j = 0; j < width; j++)
 				p[i][j] = 0;
 		}
 	}
 
 	return (p);
+}
+
+/**
+ * free_grid - Frees a 2 dimensional grid created with 'alloc_grid'
+ * @grid: Pointer to grid to free
+ * @height: Number of rows of the array
+ *
+ * Return: Nothing
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+
+	free(grid);
 }
