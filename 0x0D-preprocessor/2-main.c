06d22381229ff4f939e3f683c89249f4858180ca
joldan95 <malf95@hotmail.com>
2019-10-21 15:00:37 -0500

    [Add]Complete Task 0-4 Project 13

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+void _puts(char *str);
+
+/**
+ * main - Entry point of application
+ *
+ * Return Always 0 (Success)
+ */
+int main(void)
+{
+    _puts(__FILE__);
+    return (0);
+}
+
+/**
+ * _puts - prints a string, followed by a new line
+ * @str: String to print
+ *
+ * Return: Nothing
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (*(str + i) != '\0')
+	{
+		_putchar(*(str + i));
+		i++;
+	}
+	_putchar('\n');
+}
