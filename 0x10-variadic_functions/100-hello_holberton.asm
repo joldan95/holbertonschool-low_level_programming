69c7329e42fb1d07cc4c19d7409e8671a1971db0
joldan95 <malf95@hotmail.com>
2019-10-26 18:41:35 -0500

    [Add]Change the message write to the terminal

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -2,15 +2,15 @@
 	global main
 
 main:
-	mov edx,msglen
-	mov ecx,msg
-	mov ebx,1
-	mov eax,4
+	mov rax,4
+	mov rbx,1
+	mov rcx,msg
+	mov rdx,msglen
 	int 0x80
 
-	mov eax,1
+	mov rax,1
 	int 0x80
 
 	section .data
-msg:	db 'Hello world!', 0xa
+msg:	db 'Hello, Holberton', 0xa
 msglen:	equ $-msg
