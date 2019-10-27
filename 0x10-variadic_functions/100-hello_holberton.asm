513e17374ef74eb9851434807a49b7d44d639690
joldan95 <malf95@hotmail.com>
2019-10-27 18:14:07 -0500

    [Update]Adds 0 value to exit function

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,14 +1,15 @@
 	global main
 
 main:
-	mov rax,4
-	mov rbx,1
-	mov rcx,msg
-	mov rdx,17
+	mov rax, 4
+	mov rbx, 1
+	mov rcx, msg
+	mov rdx, 17
 	int 0x80
 
-	mov rax,1
+	mov rbx, 0
+	mov rax, 1
 	int 0x80
 
 	section .data
-msg:	db 'Hello, Holberton', 0xa
+msg:	db 'Hello, Holberton', 10
