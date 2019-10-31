a81768a528414732998ac255778e14ac8aea3328
joldan95 <malf95@hotmail.com>
2019-10-31 17:13:14 -0500

    [Fix]Style

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-void startbefore (void) __attribute__ ((constructor));
+void startbefore(void) __attribute__ ((constructor));
 
 /**
  * startbefore - Function reproduce before main
  *
  * Return: Nothing
  */
-void startbefore (void)
+void startbefore(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
