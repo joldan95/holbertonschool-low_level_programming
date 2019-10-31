8897799bcc266233bdf77af4a74bb14017758193
joldan95 <malf95@hotmail.com>
2019-10-31 17:11:36 -0500

    [Add]Complete task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void startbefore (void) __attribute__ ((constructor));
+
+/**
+ * startbefore - Function reproduce before main
+ *
+ * Return: Nothing
+ */
+void startbefore (void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
