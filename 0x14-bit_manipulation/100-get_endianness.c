48ffdbf41219758f88c684e22dc4541e93e3217c
joldan95 <malf95@hotmail.com>
2019-11-07 17:43:16 -0500

    [Add]Complete Task 6

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,15 @@
+/**
+ * get_endianness - Check the endianness
+ *
+ * Return: 0 if big endian, 1 if little endian
+ */
+int get_endianness(void)
+{
+	int i = 1;
+	char *c = (char *)&i;
+
+	if (*c)
+		return (1);
+	else
+		return (0);
+}
