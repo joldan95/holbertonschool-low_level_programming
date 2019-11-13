9ccf58930cd4adef748f4d9280357fe8d422c773
luismedinaeng <malf95@hotmail.com>
2019-11-12 21:10:51 -0500

    [Add]Complete Task 3

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,108 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include <unistd.h>
+
+void copy_textfile(char *file_from, char *file_to);
+void exit_program(int number, char *workfile);
+
+/**
+ * main - Entry point of application that copies the content of a file
+ * @ac: Count of arguments
+ * @av: List of arguments
+ *
+ * Return: Always 0 (Success)
+ * If the count of arguments is incorrect, exits the program with code 97
+ * If cannot read the origin file, exits with code 98
+ * If cannot create or write into the destiny file, exits with code 99
+ * If cannot close a file descriptor, exits with code 100
+ */
+int main(int ac, char **av)
+{
+	if (ac != 3)
+	{
+		dprintf(2, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	copy_textfile(av[1], av[2]);
+
+	return (0);
+}
+
+/**
+ * copy_textfile - Copies from text file to another
+ * @file_from: File from copy the information
+ * @file_to: File where to copy the information
+ *
+ * Description: Copies the information from one file to another
+ * If the file where to copy already exist, it truncates it and does not
+ *  change it permissions. If not, create with permissions rw-rw-r--
+ * If cannot read the file_from file, exits with code 98
+ * If cannot create or write into file_to file, exits with code 99
+ * If cannot close a file descriptor, exits with code 100
+ *
+ * Return: Nothing
+ */
+void copy_textfile(char *file_from, char *file_to)
+{
+	int fstart, fend, el1, el2, c = 1;
+	char buffer[1024];
+
+	fstart = open(file_from, O_RDONLY);
+	if (fstart == -1)
+		exit_program(98, file_from);
+
+	fend = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fend == -1)
+		close(fstart), exit_program(99, file_to);
+
+	while (c)
+	{
+		el1 = read(fstart, buffer, 1024);
+		if (el1 == -1)
+		{
+			close(fstart), close(fend);
+			exit_program(98, file_from);
+		}
+		el2 = write(fend, buffer, el1);
+		if (el2 == -1)
+		{
+			close(fstart), close(fend);
+			exit_program(99, file_to);
+		}
+		if (el2 < 1024)
+			c = 0;
+	}
+	el1 = close(fstart), el2 = close(fend);
+	if (el1 == -1 || el2 == -1)
+		exit_program(100, (el1 == -1 ? file_from : file_to));
+}
+
+/**
+ * exit_program - Exits the program and send a message according to the error
+ * @number: Number of error
+ * @workfile: Name fo the file that produces the error
+ *
+ * Return: Nothing
+ */
+void exit_program(int number, char *workfile)
+{
+	switch (number)
+	{
+	case 98:
+		dprintf(2, "Error: Can't read from file %s\n", workfile);
+		break;
+	case 99:
+		dprintf(2, "Error: Can't write to %s\n", workfile);
+		break;
+	case 100:
+		dprintf(2, "Error: Can't close fd %s\n", workfile);
+		break;
+	}
+	exit(number);
+}
