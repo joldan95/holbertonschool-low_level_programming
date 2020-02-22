0bdcba2968c0195c78fd5cc1d473fb9e5c01f76e
luismedinaeng <malf95@hotmail.com>
2020-02-21 22:49:24 -0500

    [Update]Take on count the limits of th grid as perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -17,8 +17,8 @@ def island_perimeter(grid):
     for row in range(max_row):
         for col in range(max_col):
             if grid[row][col] == 1:
-                per += 1 if row != 0 and grid[row-1][col] == 0 else 0
-                per += 1 if row != max_row - 1 and grid[row+1][col] == 0 else 0
-                per += 1 if col != 0 and grid[row][col-1] == 0 else 0
-                per += 1 if col != max_col - 1 and grid[row][col+1] == 0 else 0
+                per += 1 if row == 0 or grid[row-1][col] == 0 else 0
+                per += 1 if row == max_row - 1 or grid[row+1][col] == 0 else 0
+                per += 1 if col == 0 or grid[row][col-1] == 0 else 0
+                per += 1 if col == max_col - 1 or grid[row][col+1] == 0 else 0
     return per
